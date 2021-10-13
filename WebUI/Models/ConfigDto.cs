2c70ca94a4c73d740a450f92478da7c46aa76a9b
Emmanuel Robles <emmanuelrobles94@gmail.com>
2021-10-13 08:58:54 -0400

    Added configuration

---
+++ WebUI/Models/ConfigDto.cs
@@ -0,0 +1,9 @@
+namespace WebUI.Models
+{
+    public class ConfigDto
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public bool SlotSetting { get; set; }
+    }
+}
